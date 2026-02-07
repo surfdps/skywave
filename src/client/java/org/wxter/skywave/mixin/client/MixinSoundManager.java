71cf9e18365bc056737de91b611e00feef516d3e
Pavel <119928275+surfdps@users.noreply.github.com>
2026-02-07 14:49:50 +0300

    Fix sound manager mixin cancel handling

--- src/client/java/org/wxter/skywave/mixin/client/MixinSoundManager.java
+++ src/client/java/org/wxter/skywave/mixin/client/MixinSoundManager.java
@@ -7,6 +7,7 @@ import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.wxter.skywave.config.SkywaveConfig;
 
 @Mixin(SoundManager.class)
@@ -22,7 +23,7 @@ public class MixinSoundManager {
     );
 
     @Inject(method = "play", at = @At("HEAD"), cancellable = true)
-    private void skywave$muteEnderDragonSounds(SoundInstance sound, org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable<Boolean> cir) {
+    private void skywave$muteEnderDragonSounds(SoundInstance sound, CallbackInfo cir) {
         if (!SkywaveConfig.get().muteEnderDragonSounds) return;
 
         Identifier id = sound.getId();
@@ -30,7 +31,6 @@ public class MixinSoundManager {
 
         if (MUTED_DRAGON_SOUND_PATHS.contains(id.getPath())
                 || MUTED_DRAGON_SOUND_PATHS.contains(id.toString())) {
-            cir.setReturnValue(false);
             cir.cancel();
         }
     }
